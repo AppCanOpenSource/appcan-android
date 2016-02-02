575b2a25abf8fbf782dcf5393a264a7a8c55c08b
jiangpingping <1626794969@qq.cm>
2016-02-02 14:51:38 +0800

    添加和删除插件view时不使用Handler发送消息的方式

--- Engine/src/org/zywx/wbpalmstar/engine/EBrowserWindow.java
+++ Engine/src/org/zywx/wbpalmstar/engine/EBrowserWindow.java
@@ -202,17 +202,29 @@ public class EBrowserWindow extends SwipeView implements AnimationListener {
     }
 
     public void addViewToCurrentWindow(View child) {
-        Message msg = mWindLoop.obtainMessage();
-        msg.what = F_WHANDLER_ADD_VIEW;
-        msg.obj = child;
-        mWindLoop.sendMessage(msg);
+        //Message msg = mWindLoop.obtainMessage();
+        //msg.what = F_WHANDLER_ADD_VIEW;
+        //msg.obj = child;
+        //mWindLoop.sendMessage(msg);
+        child.setTag(EViewEntry.F_PLUGIN_VIEW_TAG);
+        Animation anim = child.getAnimation();
+        addView(child);
+        if (null != anim) {
+            anim.start();
+        }
+        bringChildToFront(child);
     }
 
     public void removeViewFromCurrentWindow(View child) {
-        Message msg = mWindLoop.obtainMessage();
-        msg.what = F_WHANDLER_REMOVE_VIEW;
-        msg.obj = child;
-        mWindLoop.sendMessage(msg);
+        //Message msg = mWindLoop.obtainMessage();
+        //msg.what = F_WHANDLER_REMOVE_VIEW;
+        //msg.obj = child;
+        //mWindLoop.sendMessage(msg);
+        Animation removeAnim = child.getAnimation();
+        if (null != removeAnim) {
+            removeAnim.start();
+        }
+        removeView(child);
     }
 
 
