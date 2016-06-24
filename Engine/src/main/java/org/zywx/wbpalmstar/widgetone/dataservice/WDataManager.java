9cb62a1512aee018d5ddf363025f754d5d90df86
蒋萍萍 <pingping.jiang@3g2win.com>
2016-06-24 16:36:58 +0800

    修复未copy完widget文件获取res://真实路径不正确的问题

--- Engine/src/main/java/org/zywx/wbpalmstar/widgetone/dataservice/WDataManager.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/widgetone/dataservice/WDataManager.java
@@ -1020,6 +1020,7 @@ public class WDataManager {
                 try {
                     CopyAssets(assetDir, dir);
                     Editor editor = m_preferences.edit();
+                    isCopyAssetsFinish = true;
                     editor.putBoolean(m_copyAssetsFinish, true);
                     editor.commit();
                 } catch (Exception e) {
