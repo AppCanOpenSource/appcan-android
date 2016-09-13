7563598c4ece6b9676cd5de49900fdb05d5cad0c
yltwust <510212753@qq.com>
2016-09-13 09:58:39 +0800

    沉浸式通知栏

--- Engine/src/org/zywx/wbpalmstar/base/util/ConfigXmlUtil.java
+++ Engine/src/org/zywx/wbpalmstar/base/util/ConfigXmlUtil.java
@@ -1,6 +1,9 @@
 package org.zywx.wbpalmstar.base.util;
 
 import android.app.Activity;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 import org.zywx.wbpalmstar.widgetone.dataservice.WWidgetData;
@@ -22,4 +25,25 @@ public class ConfigXmlUtil {
         }
     }
 
+
+    /**
+     * 设置状态栏颜色
+     */
+    public static void setStatusBarColor(Activity activity, int color){
+        if (color==-1){
+            return;
+        }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){//4.4 全透明状态栏
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0 全透明实现
+            Window window = activity.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(color);
+        }
+    }
 }
