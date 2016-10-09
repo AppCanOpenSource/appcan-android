3f9b8bdc856b781862d7ed977d9dae6e86cf44e0
蒋萍萍 <pingping.jiang@3g2win.com>
2016-10-09 16:21:33 +0800

    修改个别手机解析JS数据过大时崩溃问题

---
+++ Engine/src/main/java/org/zywx/wbpalmstar/base/vo/AppCanJsVO.java
@@ -0,0 +1,11 @@
+package org.zywx.wbpalmstar.base.vo;
+
+import java.util.List;
+
+public class AppCanJsVO {
+
+    public String uexName;
+    public String method;
+    public List<String> args;
+    public List<String> types;
+}
