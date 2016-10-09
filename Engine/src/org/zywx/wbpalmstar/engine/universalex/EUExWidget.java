de8ef2c5484f4c5d73dfc03027321e27569c684b
蒋萍萍 <pingping.jiang@3g2win.com>
2016-10-09 14:43:27 +0800

    uexWidget.startApp接口startMode为1时,extra不生效

--- Engine/src/org/zywx/wbpalmstar/engine/universalex/EUExWidget.java
+++ Engine/src/org/zywx/wbpalmstar/engine/universalex/EUExWidget.java
@@ -237,10 +237,6 @@ public class EUExWidget extends EUExBase {
             if ("0".equals(startMode)) {
                 String pkgName = params[1];
                 String clsName = null;
-                StartAppVO extraVO=null;
-                if (params.length>4) {
-                    extraVO = DataHelper.gson.fromJson(params[4],StartAppVO.class);
-                }
                 if (TextUtils.isEmpty(pkgName)) {
                     Log.e(tag, "startApp has error params!!!");
                     callBackPluginJs(JsConst.CALLBACK_START_APP, "error params");
@@ -260,14 +256,6 @@ public class EUExWidget extends EUExBase {
                 }
                 ComponentName component = new ComponentName(pkgName, clsName);
                 intent = new Intent();
-                if (extraVO!=null&&extraVO.getData()!=null){
-                    Uri contentUrl=Uri.parse(extraVO.getData());
-                    intent.setData(contentUrl);
-                }
-                // 如果isNewTask.equals("0") by waka
-                if (extraVO != null && "0".equals(extraVO.getIsNewTask())) {
-                    switchNewTask = false;// NEW_TASK开关置为false
-                }
                 intent.setComponent(component);
             } else if ("1".equals(startMode)) {
                 String action = params[1];
@@ -284,6 +272,18 @@ public class EUExWidget extends EUExBase {
                 callBackPluginJs(JsConst.CALLBACK_START_APP, "error params!");
                 return;
             }
+        }
+        StartAppVO extraVO = null;
+        if (params.length > 4) {
+            extraVO = DataHelper.gson.fromJson(params[4], StartAppVO.class);
+        }
+        if (extraVO != null && extraVO.getData() != null) {
+            Uri contentUrl = Uri.parse(extraVO.getData());
+            intent.setData(contentUrl);
+        }
+        // 如果isNewTask.equals("0") by waka
+        if (extraVO != null && "0".equals(extraVO.getIsNewTask())) {
+            switchNewTask = false;// NEW_TASK开关置为false
         }
         if (intent == null) {
             Log.e(tag, "startApp has error params!!!");
