e45e753eed3a680011b8021b83ed8cb1eb9e1ed3
yltwust <510212753@qq.com>
2016-11-02 13:33:46 +0800

    解决debug参数丢失的问题

--- Engine/src/main/java/org/zywx/wbpalmstar/widgetone/dataservice/WWidgetData.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/widgetone/dataservice/WWidgetData.java
@@ -113,7 +113,7 @@ public class WWidgetData implements Parcelable {
 
     public String m_appkey;
 
-    public int m_appdebug = 0;
+    public int m_appdebug = 0;//是否开启debug模式，0：关闭，1：开启
 
     public String m_opaque = "";
 
@@ -158,6 +158,7 @@ public class WWidgetData implements Parcelable {
             widget.m_bgColor = source.readString();
             widget.m_appkey = source.readString();
             widget.mErrorPath=source.readString();
+            widget.m_appdebug=source.readInt();
             if (widget.disablePluginsList != null) {
                 source.readStringList(widget.disablePluginsList);
             }
@@ -233,6 +234,7 @@ public class WWidgetData implements Parcelable {
         parcel.writeString(m_bgColor);
         parcel.writeString(m_appkey);
         parcel.writeString(mErrorPath);
+        parcel.writeInt(m_appdebug);
         parcel.writeStringList(disablePluginsList);
         parcel.writeStringList(disableRootWindowsList);
         parcel.writeStringList(disableSonWindowsList);
