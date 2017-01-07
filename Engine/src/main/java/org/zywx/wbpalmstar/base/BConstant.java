eb15e05e398511d84f5b797a49de7232fd692499
ylt <510212753@qq.com>
2017-01-07 17:34:11 +0800

    升级版本号

--- Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
@@ -25,8 +25,8 @@ import java.text.DecimalFormat;
 
 public class BConstant {
 
-    public static final String ENGINE_VERSION="4.1.0";
-    public static final int ENGINE_VERSION_CODE=41000;
+    public static final String ENGINE_VERSION="4.1.1";
+    public static final int ENGINE_VERSION_CODE=41001;
 
     public static final String F_URL = "url";
     public static final String F_WIDGET = "widget";
