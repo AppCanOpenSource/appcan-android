c04fa37df37141013da715d0d70e7735901a7bf5
ylt <510212753@qq.com>
2017-01-11 14:26:42 +0800

    格式化代码

--- Engine/src/main/assets/widget/js/debug.js
+++ Engine/src/main/assets/widget/js/debug.js
@@ -1,59 +1,59 @@
 var uexCallback = {
-  queue: [],
-  callback: function() {
-    var params = Array.prototype.slice.call(arguments, 0);
-    var id = params.shift();
-    var permanent = params.shift();
-    this.queue[id].apply(this, params);
-    if (!permanent) {
-      delete this.queue[id];
+    queue: [],
+    callback: function () {
+        var params = Array.prototype.slice.call(arguments, 0);
+        var id = params.shift();
+        var permanent = params.shift();
+        this.queue[id].apply(this, params);
+        if (!permanent) {
+            delete this.queue[id];
+        }
     }
-  }
 };
 
 function fo() {
-  var args_all = Array.prototype.slice.call(arguments, 0);
-  var uexName = args_all[0];
-  var method = args_all[1];
-  var args = Array.prototype.slice.call(args_all[2], 0);
-  var aTypes = [];
-  for (var i = 0; i < args.length; i++) {
-    var arg = args[i];
-    var type = typeof arg;
-    if (type == "function") {
-      var callbackID = uexCallback.queue.length;
-      uexCallback.queue[callbackID] = arg;
-      args[i] = callbackID;
-    }
-    aTypes[aTypes.length] = type;
-  }
-  var result = JSON.parse(prompt('AppCan_onJsParse:' + JSON.stringify({
-    uexName: uexName,
-    method: method,
-    args: args,
-    types: aTypes
-  })));
-  if (result.code != 200) {
-    console.log("method call error, code:" + result.code + ", message: " +
-      result.result);
-  }
-  var content = result.result;
-  if (content.funcInfos != null) {
-    for (var name in content.funcInfos) {
-      var targetFunc = content.funcInfos[name];
-      content[name] = function() {
-        var params = Array.prototype.slice.call(arguments, 0);
-        if (content.ext != null) {
-          params[params.length] = content.ext;
+    var args_all = Array.prototype.slice.call(arguments, 0);
+    var uexName = args_all[0];
+    var method = args_all[1];
+    var args = Array.prototype.slice.call(args_all[2], 0);
+    var aTypes = [];
+    for (var i = 0; i < args.length; i++) {
+        var arg = args[i];
+        var type = typeof arg;
+        if (type == "function") {
+            var callbackID = uexCallback.queue.length;
+            uexCallback.queue[callbackID] = arg;
+            args[i] = callbackID;
         }
-        window[uexName][targetFunc].apply(uexName, params);
-      };
+        aTypes[aTypes.length] = type;
     }
-    delete content.funcInfos;
-  }
-  return content;
+    var result = JSON.parse(prompt('AppCan_onJsParse:' + JSON.stringify({
+            uexName: uexName,
+            method: method,
+            args: args,
+            types: aTypes
+        })));
+    if (result.code != 200) {
+        console.log("method call error, code:" + result.code + ", message: " +
+            result.result);
+    }
+    var content = result.result;
+    if (content.funcInfos != null) {
+        for (var name in content.funcInfos) {
+            var targetFunc = content.funcInfos[name];
+            content[name] = function () {
+                var params = Array.prototype.slice.call(arguments, 0);
+                if (content.ext != null) {
+                    params[params.length] = content.ext;
+                }
+                window[uexName][targetFunc].apply(uexName, params);
+            };
+        }
+        delete content.funcInfos;
+    }
+    return content;
 }
 window.uexDispatcher = {};
-uexDispatcher.dispatch = function() {
-  return fo(arguments[0], arguments[1], arguments[2]);
+uexDispatcher.dispatch = function () {
+    return fo(arguments[0], arguments[1], arguments[2]);
 };
--- Engine/src/main/assets/widget/js/main.js
+++ Engine/src/main/assets/widget/js/main.js
@@ -1,10 +1,9 @@
-function openNewWin(inWndName,html,inAniID,f,time)
-{
+function openNewWin(inWndName, html, inAniID, f, time) {
     f = 1024;
-	if (inAniID) {
-		uexWindow.open(inWndName,'0',html,inAniID,'','',(f)?f:4,time?time:250);
-	}
-	else {
-		uexWindow.open(inWndName, '0', html, '2', '', '', (f) ? f : 4);
-	}
+    if (inAniID) {
+        uexWindow.open(inWndName, '0', html, inAniID, '', '', (f) ? f : 4, time ? time : 250);
+    }
+    else {
+        uexWindow.open(inWndName, '0', html, '2', '', '', (f) ? f : 4);
+    }
 }
