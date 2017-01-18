9509f0d050fa51b5c96cd0226325455ddabba564
蒋萍萍 <pingping.jiang@3g2win.com>
2017-01-18 11:49:29 +0800

    修改推送相关问题

--- Engine/src/main/java/org/zywx/wbpalmstar/engine/universalex/EUExWidget.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/engine/universalex/EUExWidget.java
@@ -235,7 +235,7 @@ public class EUExWidget extends EUExBase {
     public void delPushInfo(String[] params) {
         String host_pushBindUser = ResoureFinder.getInstance().getString(mContext,
                 "bindUser_host");
-        if (host_pushBindUser.indexOf("push") > 0) {
+        if (host_pushBindUser.indexOf("push") > 0 || host_pushBindUser.indexOf("mms") > 0) {
             String uId = "";
             String uNickName = "";
             if (params.length > 0) {
@@ -780,7 +780,7 @@ public class EUExWidget extends EUExBase {
         final String userNick = parm[1];
         String host_pushBindUser = ResoureFinder.getInstance().getString(mContext,
                 "bindUser_host");
-        if (host_pushBindUser.indexOf("push") > 0) {
+        if (host_pushBindUser.indexOf("push") > 0 || host_pushBindUser.indexOf("mms") > 0) {
             AppCan.getInstance().setPushInfo(userId, userNick, mContext, mBrwView);
         } else {
             AppCan.getInstance().deviceBind(userId, userNick, mContext, mBrwView);
