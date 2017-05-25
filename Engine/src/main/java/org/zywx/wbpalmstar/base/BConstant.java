5bf07ee2276a21fe20c9058345c313f8a2110359
AppCanOpenSource <guangqiao.li@3g2win.com>
2017-05-25 17:49:22 +0800

    升级版本号

--- Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
@@ -25,7 +25,7 @@ import java.text.DecimalFormat;
 
 public class BConstant {
 
-    public static final String ENGINE_VERSION="4.1.5";
+    public static final String ENGINE_VERSION="4.1.6";
     public static final int ENGINE_VERSION_CODE=41005;
 
     public static final String F_URL = "url";
