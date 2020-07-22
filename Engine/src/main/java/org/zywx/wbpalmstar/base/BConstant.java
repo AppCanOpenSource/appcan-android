44ff30bb96d4ab833467b7debc76eca9b1a9edbb
张一鹏 <yipeng.zhang@zymobi.com>
2020-07-22 17:59:06 +0800

    update version 4.4.33

--- Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
@@ -25,8 +25,8 @@ import java.text.DecimalFormat;
 
 public class BConstant {
 
-    public static final String ENGINE_VERSION="4.4.32";
-    public static final int ENGINE_VERSION_CODE=404032;
+    public static final String ENGINE_VERSION="4.4.33";
+    public static final int ENGINE_VERSION_CODE=404033;
 
     public static final String F_URL = "url";
     public static final String F_WIDGET = "widget";
