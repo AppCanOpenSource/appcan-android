8ebe3151c4d09e65fde3d36df43afb72fc5adeb0
张一鹏 <yipeng.zhang@zymobi.com>
2021-03-10 19:50:16 +0800

    修复证书链为1层时跳过了证书校验的安全错误
    (cherry picked from commit 3af48cb98c4d3afcd70a6eae4345c2acdb022b53)
    (cherry picked from commit 2dc103dc727376b9cdfd32e732294d438df149e4)

--- Engine/src/main/java/org/zywx/wbpalmstar/platform/certificates/HX509TrustManager.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/platform/certificates/HX509TrustManager.java
@@ -1,71 +1,57 @@
-/*
- *  Copyright (C) 2014 The AppCan Open Source Project.
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
-
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
-
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package org.zywx.wbpalmstar.platform.certificates;
 
-
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
+import org.zywx.wbpalmstar.base.BDebug;
 
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
 public class HX509TrustManager implements X509TrustManager {
 
-    private static X509TrustManager mDefaultTrustManager;
-    private X509TrustManager mTrustManager;
+    private static final String TAG = "HX509TrustManager";
+
+    private volatile static X509TrustManager mDefaultTrustManager;
+    private final X509TrustManager mTrustManager;
 
     public HX509TrustManager(KeyStore ksP12) throws Exception {
-
-        TrustManagerFactory tfactory = TrustManagerFactory.getInstance(Http.algorithm);
-        tfactory.init(ksP12);
-        TrustManager[] trustMgr = tfactory.getTrustManagers();
+        TrustManagerFactory tFactory = TrustManagerFactory
+                .getInstance(Http.algorithm);
+        tFactory.init(ksP12);
+        if (ksP12 != null){
+            BDebug.i(TAG, "ksP12 size is " + ksP12.size());
+        }
+        TrustManager[] trustMgr = tFactory.getTrustManagers();
         if (trustMgr.length == 0) {
             throw new NoSuchAlgorithmException("no trust manager found");
         }
         mTrustManager = (X509TrustManager) trustMgr[0];
         if (mDefaultTrustManager == null) {
-            // 初始化系统默认证书库的trustManager
-            TrustManagerFactory defaultTfactory = TrustManagerFactory
-                    .getInstance(Http.algorithm);
-            defaultTfactory.init((KeyStore) null);
-            TrustManager[] defaultTrustMgr = defaultTfactory.getTrustManagers();
-            if (defaultTrustMgr.length == 0) {
-                throw new NoSuchAlgorithmException(
-                        "no default trust manager found");
+            synchronized (HX509TrustManager.class) {
+                if (mDefaultTrustManager == null) {
+                    // 初始化系统默认证书库的trustManager
+                    TrustManagerFactory defaultTFactory = TrustManagerFactory
+                            .getInstance(Http.algorithm);
+                    defaultTFactory.init((KeyStore) null);
+                    TrustManager[] defaultTrustMgr = defaultTFactory.getTrustManagers();
+                    if (defaultTrustMgr.length == 0) {
+                        throw new NoSuchAlgorithmException(
+                                "no default trust manager found");
+                    }
+                    mDefaultTrustManager = (X509TrustManager) defaultTrustMgr[0];
+                }
             }
-            mDefaultTrustManager = (X509TrustManager) defaultTrustMgr[0];
         }
     }
 
     @Override
-    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        if (Http.isCheckTrustCert()) {
-            try {
-                mTrustManager.checkClientTrusted(chain, authType);
-            } catch (Exception e) {
-                e.printStackTrace();
-                mDefaultTrustManager.checkClientTrusted(chain, authType);
-            }
-        }
+    public void checkClientTrusted(X509Certificate[] chain, String authType)
+            throws CertificateException {
+        BDebug.e(TAG, "checkClientTrusted: this method should not be invoked.");
     }
 
     @Override
@@ -73,14 +59,32 @@ public class HX509TrustManager implements X509TrustManager {
             throws CertificateException {
         if (Http.isCheckTrustCert()) {
             try {
-                if ((chain != null) && (chain.length == 1)) {
-                    chain[0].checkValidity();
-                } else {
-                    mTrustManager.checkServerTrusted(chain, authType);
+                // 验证当前时间是否在证书链中证书的有效期内
+                if(chain != null){
+                    BDebug.i(TAG, "checkServerTrusted:  ServerCertificateChain Size is " + chain.length);
+                    for (X509Certificate cert : chain){
+                        cert.checkValidity();
+                        if (BDebug.isDebugMode()) {
+                            String certStr = "\nSubjectDN:" + cert.getSubjectDN().getName()
+                                    + "\nIssuerDN:" + cert.getIssuerDN().getName()
+                                    + "\nNotBefore:" + cert.getNotBefore()
+                                    + "\nNotAfter:" + cert.getNotAfter();
+                            BDebug.i(TAG, "checkServerTrusted:  " + certStr);
+                        }
+                    }
                 }
+                // 验证证书是否可信
+                mTrustManager.checkServerTrusted(chain, authType);
             } catch (Exception e) {
-                e.printStackTrace();
-                mDefaultTrustManager.checkServerTrusted(chain, authType);
+                try {
+                    mDefaultTrustManager.checkServerTrusted(chain, authType);
+                } catch (CertificateException certificateException) {
+                    // 系统信任区也失败的话，就抛出异常。抛出之前先打印一下上一个错误。
+                    if (BDebug.isDebugMode()){
+                        e.printStackTrace();
+                    }
+                    throw certificateException;
+                }
             }
         }
     }
@@ -88,6 +92,15 @@ public class HX509TrustManager implements X509TrustManager {
     @Override
     public X509Certificate[] getAcceptedIssuers() {
         X509Certificate[] certs = mTrustManager.getAcceptedIssuers();
+        if (BDebug.isDebugMode()) {
+            if (certs != null){
+                BDebug.i(TAG, "getAcceptedIssuers certs size is " + certs.length);
+                for (X509Certificate cert : certs) {
+                    String certStr = "S:" + cert.getSubjectDN().getName() + "\nI:" + cert.getIssuerDN().getName();
+                    BDebug.i(TAG, "getAcceptedIssuers:  " + certStr);
+                }
+            }
+        }
         return certs;
     }
 
