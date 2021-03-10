3af48cb98c4d3afcd70a6eae4345c2acdb022b53
张一鹏 <yipeng.zhang@zymobi.com>
2021-03-10 19:50:16 +0800

    修复证书链为1层时跳过了证书校验的安全错误

--- Engine/src/main/java/org/zywx/wbpalmstar/platform/certificates/HNetSSLSocketFactory.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/platform/certificates/HNetSSLSocketFactory.java
@@ -1,24 +1,4 @@
-/*
- *  Copyright (C) 2014 The AppCan Open Source Project.
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
-
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
-
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package org.zywx.wbpalmstar.platform.certificates;
-
-import javax.net.ssl.*;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -27,11 +7,18 @@ import java.net.UnknownHostException;
 import java.security.KeyStore;
 import java.security.SecureRandom;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+
 public class HNetSSLSocketFactory extends SSLSocketFactory {
 
-    private SSLContext mSSLContext;
+    private final SSLContext mSSLContext;
 
-    public HNetSSLSocketFactory(KeyStore ksP12, String keyPass) throws Exception {
+    public HNetSSLSocketFactory(KeyStore ksP12, String keyPass)
+            throws Exception {
         super();
         mSSLContext = SSLContext.getInstance("TLS");
         KeyManagerFactory kMgrFact = null;
@@ -46,54 +33,62 @@ public class HNetSSLSocketFactory extends SSLSocketFactory {
             kMgrFact.init(ksP12, null);
         }
         kMgrs = kMgrFact.getKeyManagers();
-        tMgrs = new TrustManager[]{tMgr};
-        SecureRandom secureRandom = new java.security.SecureRandom();
+        tMgrs = new TrustManager[] { tMgr };
+        SecureRandom secureRandom = new SecureRandom();
         mSSLContext.init(kMgrs, tMgrs, secureRandom);
     }
 
     @Override
     public String[] getDefaultCipherSuites() {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         return socketfact.getDefaultCipherSuites();
     }
 
     @Override
     public String[] getSupportedCipherSuites() {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         return socketfact.getSupportedCipherSuites();
     }
 
     @Override
-    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+    public Socket createSocket(Socket socket, String host, int port,
+                               boolean autoClose) throws IOException {
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         Socket result = socketfact.createSocket(socket, host, port, autoClose);
         return result;
     }
 
     @Override
-    public Socket createSocket(String host, int port) throws IOException,
-            UnknownHostException {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+    public Socket createSocket(String host, int port)
+            throws IOException, UnknownHostException {
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         return socketfact.createSocket(host, port);
     }
 
     @Override
     public Socket createSocket(String host, int port, InetAddress localHost,
                                int localPort) throws IOException, UnknownHostException {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         return socketfact.createSocket(host, port, localHost, localPort);
     }
 
     @Override
     public Socket createSocket(InetAddress host, int port) throws IOException {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         return socketfact.createSocket(host, port);
     }
 
     @Override
     public Socket createSocket(InetAddress address, int port,
                                InetAddress localAddress, int localPort) throws IOException {
-        javax.net.ssl.SSLSocketFactory socketfact = mSSLContext.getSocketFactory();
+        SSLSocketFactory socketfact = mSSLContext
+                .getSocketFactory();
         return socketfact.createSocket(address, port, localAddress, localPort);
     }
 
