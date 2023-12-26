95920b0cc554295a5a99f6887e814c0fa269bcef
望山观海 <sandy1108@163.com>
2023-12-27 00:12:15 +0800

    update version 4.7.49 beta

--- Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
@@ -25,7 +25,7 @@ import java.text.DecimalFormat;
 
 public class BConstant {
 
-    public static final String ENGINE_VERSION="4.6.45";
+    public static final String ENGINE_VERSION="4.7.49";
     public static final int ENGINE_VERSION_CODE=406045;
 
     /**
