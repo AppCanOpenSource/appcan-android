a593679f4edb65c62ccac01b922c3ca14827a17a
望山观海 <sandy1108@163.com>
2024-01-25 21:50:57 +0800

    update version 4.7.50

--- Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
+++ Engine/src/main/java/org/zywx/wbpalmstar/base/BConstant.java
@@ -25,8 +25,8 @@ import java.text.DecimalFormat;
 
 public class BConstant {
 
-    public static final String ENGINE_VERSION="4.7.49";
-    public static final int ENGINE_VERSION_CODE=407049;
+    public static final String ENGINE_VERSION="4.7.50";
+    public static final int ENGINE_VERSION_CODE=407050;
 
     /**
      * 当前使用的softToken生成规则的版本，用于做兼容和更新的判断
